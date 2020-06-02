6fc82079e6f339bf28140c90974443b4f6a8d51e
CaddyDz <caddydz4@gmail.com>
2020-06-02 23:21:19 +0100

    Sort imports
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- app/User.php
+++ app/User.php
@@ -4,8 +4,8 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
