8ffc3787d712816e36245be4b05e4b746f91bf45
CaddyDz <caddydz4@gmail.com>
2020-06-02 22:34:02 +0100

    Created Profile Model for extra user data
    * Moved Avatar attribute to profile
    
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

---
+++ app/Profile.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Spatie\MediaLibrary\HasMedia;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class Profile extends Model implements HasMedia
+{
+    use InteractsWithMedia;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('account_menu')
+            ->width(44)
+            ->height(44)
+            ->sharpen(10);
+    }
+}
