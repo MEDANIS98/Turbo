21809114a8ad550ffdbf388a2de875977040e596
CaddyDz <caddydz4@gmail.com>
2020-06-03 13:47:51 +0100

    Set profile object globally
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -12,7 +12,7 @@ class ProfileTest extends TestCase
 {
     use DatabaseMigrations;
 
-    private object $user;
+    private object $user, $profile;
 
     public function setUp(): void
     {
@@ -20,6 +20,9 @@ class ProfileTest extends TestCase
         Storage::disk('public')->deleteDirectory('avatars');
         Storage::disk('public')->makeDirectory('avatars');
         $this->user = create(User::class);
+        $this->profile = create(Profile::class, [
+            'user_id' => $this->user->id
+        ]);
     }
 
     /**
@@ -30,10 +33,7 @@ class ProfileTest extends TestCase
     public function testProfileAvatar(): void
     {
         // Create a profile and set the user id to the user in the constructor
-        $profile = create(Profile::class, [
-            'user_id' => $this->user->id
-        ]);
-        $profile->addMedia(storage_path('app/public/' . $profile->avatar))
+        $this->profile->addMedia(storage_path('app/public/' . $this->profile->avatar))
             ->toMediaCollection();
         $this->assertNotEquals('/images/avatar.png', $this->user->avatar);
         $this->assertNotEquals('/images/avatar.png', $this->user->accountMenuAvatar);
