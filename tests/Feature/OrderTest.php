df2d08a0fac5f652941e8885dfb1a367d83a9872
CaddyDz <caddydz4@gmail.com>
2020-06-03 14:13:34 +0100

    Test Order User relationship
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

---
+++ tests/Feature/OrderTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use App\Order;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class OrderTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * Test User Relationship
+     *
+     * Assert that the user property on the Order model returns an object
+     * that is an instance of the User model
+     *
+     *
+     * @return void
+     **/
+    public function testUserRelationship(): void
+    {
+        $user = create(User::class);
+        $order = create(Order::class, ['user_id' => $user->id]);
+        $this->assertInstanceOf(User::class, $order->user);
+    }
+}
