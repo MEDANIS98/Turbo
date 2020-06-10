83e54daea4cd5c27f09f9ca5b3d655c8ef6de792
CaddyDz <caddydz4@gmail.com>
2020-06-10 20:41:29 +0100

    Removed redundant code
    Closes #172
    
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,15 +36,7 @@
                                     class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="password" type="password"
-                                        class="form-control @error('password') is-invalid @enderror" name="password"
-                                        required autocomplete="current-password">
-
-                                    @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
+                                    <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password">
                                 </div>
                             </div>
 
@@ -66,12 +58,9 @@
                                     <button type="submit" class="btn btn-primary">
                                         {{ __('Login') }}
                                     </button>
-
-                                    @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
-                                    @endif
                                 </div>
                             </div>
                         </form>
