1d13268cbdb9f630e053ef7b5e3fd3431e3a48da
CaddyDz <caddydz4@gmail.com>
2020-06-12 18:46:31 +0100

    Added dev route to test mailables
    Closes #169
    
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

---
+++ routes/dev.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Dev Routes
+|--------------------------------------------------------------------------
+|
+| These are routes purely for testing and are active only in development
+| environment, as this file is read and routes mapped on local env.
+|
+| Make sure to only use Closures and don't call any controller functions.
+|
+*/
+
+Route::get('redis', function () {
+	\Illuminate\Support\Facades\Redis::set('name', 'Caddy');
+
+	$values = \Illuminate\Support\Facades\Redis::get('name');
+	return $values;
+});
+
+Route::get('part', function () {
+	$part = App\Part::find(1);
+
+	$vehicle = $part->vehicle->only('year', 'brand', 'model', 'fuel');
+
+	$part = $part->only('title');
+
+	$searchable = array_merge($part, $vehicle);
+
+	return $searchable;
+});
+
+Route::view('search', 'search.results');
+
+
+Route::get('mailable', function () {
+	$array = [
+		"name" => "Mr. Barry Roob Sr.",
+		"email" => "kkris@gmail.com",
+		"subject" => "However, I've got.",
+		"message" => "Mouse, in a minute, trying to put everything.",
+	];
+	return new App\Mail\ContactMail($array);
+});
