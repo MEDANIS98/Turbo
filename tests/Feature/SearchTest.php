106c46c33c0bf54a671fc4de6554ec60514b4f53
CaddyDz <caddydz4@gmail.com>
2020-06-13 10:31:34 +0100

    Apply CS fixes
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- tests/Feature/SearchTest.php
+++ tests/Feature/SearchTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Feature;
 
 use App\Vehicle;
@@ -8,24 +10,24 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class SearchTest extends TestCase
 {
-	use DatabaseMigrations;
+    use DatabaseMigrations;
 
-	/**
-	 * Test users can search for parts
-	 * of a given vehicle criteria
-	 *
-	 * @return void
-	 */
-	public function test_vehicle_parts_search()
-	{
-		$vehicle = factory(Vehicle::class)->create();
-		$response = $this->post('/search', [
-			'year' => $vehicle->year,
-			'brand' => $vehicle->brand,
-			'model' => $vehicle->model,
-			'fuel' => $vehicle->fuel,
-		]);
+    /**
+     * Test users can search for parts
+     * of a given vehicle criteria.
+     *
+     * @return void
+     */
+    public function test_vehicle_parts_search()
+    {
+        $vehicle = factory(Vehicle::class)->create();
+        $response = $this->post('/search', [
+            'year' => $vehicle->year,
+            'brand' => $vehicle->brand,
+            'model' => $vehicle->model,
+            'fuel' => $vehicle->fuel,
+        ]);
 
-		$response->assertOk();
-	}
+        $response->assertOk();
+    }
 }
