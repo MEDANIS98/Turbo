745544cf94f66817a05c907eeea3e67310440a1f
CaddyDz <caddydz4@gmail.com>
2020-06-13 10:32:36 +0100

    Added vehicle factory
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

---
+++ database/factories/VehicleFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Vehicle;
+use Faker\Provider\Fakecar;
+use Faker\Generator as Faker;
+
+$factory->define(Vehicle::class, function (Faker $faker) {
+	$faker->addProvider(new Fakecar($faker));
+    $v = $faker->vehicleArray();
+    return [
+        'year'  => $faker->biasedNumberBetween(1998, 2020, 'sqrt'),
+        'brand' => $v['brand'],
+        'model' => $v['model'],
+        'fuel'  => $faker->vehicleFuelType,
+    ];
+});
