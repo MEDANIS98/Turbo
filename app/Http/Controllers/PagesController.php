6c5f2d27d913fc7b936b99e8d1a4f951d33b0e34
CaddyDz <caddydz4@gmail.com>
2020-06-13 09:48:06 +0100

    Added pages controller
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Vehicle;
+use Illuminate\View\View;
+
+class PagesController extends Controller
+{
+    /**
+	 * Index page
+	 *
+	 * Return the index view
+	 * compacting data that includes
+	 * Distinct vehicle years, brands, models and fuel types
+	 *
+	 * @return \Illuminate\View\View $view The index view
+	 **/
+	public function index(): View
+	{
+		$years = Vehicle::select('year')->distinct()->orderBy('year')->pluck('year');
+		return view('index', compact('years'));
+	}
+}
