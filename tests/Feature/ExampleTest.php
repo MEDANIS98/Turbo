bc72a25cc00242740e5fddb99ad904a256344f7f
CaddyDz <caddydz4@gmail.com>
2020-06-13 10:04:05 +0100

    Use database migration trait on example test
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseMigrations;
+
     /**
      * A basic test example.
      *
