974525acd34699d3142ecb2d3d929b8412850d71
CaddyDz <caddydz4@gmail.com>
2020-06-13 09:53:43 +0100

    Apply CS fixes
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- app/Vehicle.php
+++ app/Vehicle.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
 
 use Laravel\Scout\Searchable;
@@ -8,59 +10,54 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Vehicle extends Model
 {
-	use Searchable;
+    use Searchable;
 
-	/**
-	 * Get the index name for the model.
-	 *
-	 * @return string
-	 */
-	public function searchableAs()
-	{
-		return 'vehicles_index';
-	}
+    /**
+     * Get the index name for the model.
+     *
+     * @return string
+     */
+    public function searchableAs()
+    {
+        return 'vehicles_index';
+    }
 
-	/**
-	 * Get the indexable data array for the model.
-	 *
-	 * @return array
-	 */
-	public function toSearchableArray()
-	{
-		$array = $this->only('brand', 'model', 'year', 'fuel');
+    /**
+     * Get the indexable data array for the model.
+     *
+     * @return array
+     */
+    public function toSearchableArray()
+    {
+        $array = $this->only('brand', 'model', 'year', 'fuel');
 
-		return $array;
-	}
+        return $array;
+    }
 
-	/**
-	 * Get the value used to index the model.
-	 *
-	 * @return mixed
-	 */
-	public function getScoutKey()
-	{
-		return $this->model;
-	}
+    /**
+     * Get the value used to index the model.
+     */
+    public function getScoutKey()
+    {
+        return $this->model;
+    }
 
-	/**
-	 * Get the key name used to index the model.
-	 *
-	 * @return mixed
-	 */
-	public function getScoutKeyName()
-	{
-		return 'model';
-	}
+    /**
+     * Get the key name used to index the model.
+     */
+    public function getScoutKeyName()
+    {
+        return 'model';
+    }
 
-	/**
-	 * Get parts of vehicle
-	 *
-	 * Defines the hasMany relationship
-	 *
-	 * @return \Illuminate\Database\Eloquent\Relations\HasMany
-	 **/
-	public function parts(): HasMany
-	{
-		return $this->hasMany(Part::class);
-	}
+    /**
+     * Get parts of vehicle.
+     *
+     * Defines the hasMany relationship
+     *
+     **/
+    public function parts(): HasMany
+    {
+        return $this->hasMany(Part::class);
+    }
 }
