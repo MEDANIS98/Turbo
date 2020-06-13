931ac10c6d5acf358936e1b22ede8437ff6ab645
CaddyDz <caddydz4@gmail.com>
2020-06-13 20:59:29 +0100

    Added Nova files
    * Ignored them by mistake
    
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

---
+++ app/Nova/Option.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Nova;
+
+use Spatie\Translatable\HasTranslations;
+use Whitecube\NovaPage\Pages\OptionResource;
+
+class Option extends OptionResource
+{
+	use HasTranslations;
+	/**
+	 * Get the displayable label of the resource.
+	 *
+	 * @return string
+	 */
+	public static function label()
+	{
+		return __(config('novapage.labels.options'));
+	}
+
+	/**
+	 * Get the displayable singular label of the resource.
+	 *
+	 * @return string
+	 */
+	public static function singularLabel()
+	{
+		return __(config('novapage.labels.option'));
+	}
+}
