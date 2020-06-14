356a591b4d6bc5cd5b140afdf5a0db1971ff33b7
CaddyDz <caddydz4@gmail.com>
2020-06-14 12:09:04 +0100

    Customize file paths
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,45 +6,42 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $this->clearSearchIndexes();
-        $this->cleanupStorage();
-        $this->call(UserSeeder::class);
-        $this->call(VehicleSeeder::class);
-    }
+	/**
+	 * Seed the application's database.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		$this->clearSearchIndexes();
+		$this->cleanupStorage();
+		$this->call(UserSeeder::class);
+		$this->call(VehicleSeeder::class);
+	}
 
-    /**
-     * Clear search indexes.
-     *
-     * Calls scout flush artisan command
-     *
-     **/
-    private function clearSearchIndexes(): void
-    {
-        Artisan::call('scout:flush App\\\Vehicle');
-        Artisan::call('scout:flush App\\\Part');
-    }
+	/**
+	 * Clear search indexes.
+	 *
+	 * Calls scout flush artisan command
+	 *
+	 **/
+	private function clearSearchIndexes(): void
+	{
+		Artisan::call('scout:flush App\\\Vehicle');
+		Artisan::call('scout:flush App\\\Part');
+	}
 
-    /**
-     * Cleanup storage before seeding.
-     *
-     * Delete and recreate storage directories
-     *
-     **/
-    public function cleanupStorage(): void
-    {
-        for ($i = 1; $i < 50; $i++) {
-            Storage::disk('public')->deleteDirectory($i);
-        }
-        Storage::disk('public')->deleteDirectory('parts');
-        Storage::disk('public')->makeDirectory('parts');
-        Storage::disk('public')->deleteDirectory('avatars');
-        Storage::disk('public')->makeDirectory('avatars');
-    }
+	/**
+	 * Cleanup storage before seeding.
+	 *
+	 * Delete and recreate storage directories
+	 *
+	 **/
+	public function cleanupStorage(): void
+	{
+		Storage::disk('public')->deleteDirectory('parts');
+		Storage::disk('public')->makeDirectory('parts');
+		Storage::disk('public')->deleteDirectory('avatars');
+		Storage::disk('public')->makeDirectory('avatars');
+	}
 }
