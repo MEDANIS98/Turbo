6ea649641f307c6c5a4c918bc4dee22406d80fe5
CaddyDz <caddydz4@gmail.com>
2020-06-25 18:14:23 +0100

    Seeded real brands with images
    * Images are in the ignored data folder
    
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

---
+++ database/seeds/BrandSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Brand;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
+
+class BrandSeeder extends Seeder
+{
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		Storage::disk('public')->deleteDirectory('brands');
+		Storage::disk('public')->makeDirectory('brands');
+		require_once(dirname(__FILE__) . '/data/brands.php');
+		foreach ($brands as $name => $country) {
+			copy(
+				base_path("data/brands/$name.png"),
+				storage_path("app/public/brands/$name.png")
+			);
+			Brand::create([
+				'name' => $name,
+				'country' => $country,
+				'logo' => "brands/$name.png",
+			]);
+		}
+	}
+}
---
+++ database/seeds/data/brands.php
@@ -0,0 +1,5 @@
+<?php
+
+$brands = [
+	'Audi' => 'Germany'
+];
