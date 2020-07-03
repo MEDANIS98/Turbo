c988de3872017e3f6d71d81250acf0c051306e6c
CaddyDz <caddydz4@gmail.com>
2020-07-03 16:47:36 +0100

    Apply CS fixes
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- app/Http/Controllers/WorkshopController.php
+++ app/Http/Controllers/WorkshopController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
 use App\Workshop;
@@ -7,79 +9,73 @@ use Illuminate\Http\Request;
 
 class WorkshopController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		//
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		//
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		//
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Workshop  $workshop
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Workshop $workshop)
-    {
-        //
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show(Workshop $workshop)
+	{
+		//
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Workshop  $workshop
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Workshop $workshop)
-    {
-        //
-    }
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit(Workshop $workshop)
+	{
+		//
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Workshop  $workshop
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Workshop $workshop)
-    {
-        //
-    }
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, Workshop $workshop)
+	{
+		//
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Workshop  $workshop
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Workshop $workshop)
-    {
-        //
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy(Workshop $workshop)
+	{
+		//
+	}
 }
