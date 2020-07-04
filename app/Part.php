5c440c6c1c38e035fa9c702f2099c6aa49541077
CaddyDz <caddydz4@gmail.com>
2020-07-04 12:07:36 +0100

    Part view
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- app/Part.php
+++ app/Part.php
@@ -166,7 +166,7 @@ class Part extends Model implements HasMedia, Buyable
 	public function getIndexImageAttribute(): string
 	{
 		$mediaItems = $this->getMedia();
-		if (! empty($mediaItems)) {
+		if (!empty($mediaItems)) {
 			return $mediaItems[0]->getUrl('_245x245');
 		}
 
@@ -176,7 +176,7 @@ class Part extends Model implements HasMedia, Buyable
 	public function getCartHeaderImageAttribute(): string
 	{
 		$mediaItems = $this->getMedia();
-		if (! empty($mediaItems)) {
+		if (!empty($mediaItems)) {
 			return $mediaItems[0]->getUrl('_70x70');
 		}
 
@@ -186,7 +186,7 @@ class Part extends Model implements HasMedia, Buyable
 	public function getNewArrivalImageAttribute(): string
 	{
 		$mediaItems = $this->getMedia();
-		if (! empty($mediaItems)) {
+		if (!empty($mediaItems)) {
 			try {
 				return $mediaItems[0]->getUrl('_92x92');
 			} catch (Exception $ex) {
@@ -207,5 +207,10 @@ class Part extends Model implements HasMedia, Buyable
 	public function invoices()
 	{
 		return $this->belongsToMany(Invoice::class);
+	}
+
+	public function brand()
+	{
+		return $this->belongsTo(Brand::class, 'vehicle_id', 'id', Vehicle::class);
 	}
 }
