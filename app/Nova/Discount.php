67220f68729d47582fdc26ce2654d01e81102e14
CaddyDz <caddydz4@gmail.com>
2020-07-13 10:05:13 +0100

    Discount base architeture
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

---
+++ app/Nova/Discount.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\DateTime;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Number;
+use Laravel\Nova\Fields\Text;
+
+class Discount extends Resource
+{
+	/**
+	 * The model the resource corresponds to.
+	 *
+	 * @var string
+	 */
+	public static $model = 'App\Discount';
+
+	/**
+	 * The single value that should be used to represent the resource when being displayed.
+	 *
+	 * @var string
+	 */
+	public static $title = 'id';
+
+	/**
+	 * The columns that should be searched.
+	 *
+	 * @var array
+	 */
+	public static $search = [
+		'id',
+	];
+
+	/**
+	 * Get a fresh instance of the model represented by the resource.
+	 */
+	public static function newModel()
+	{
+		$model = static::$model;
+		$discount = new $model;
+		// Set the dafault value for the reception date
+		$discount->percentage = 10;
+		return $discount;
+	}
+
+	/**
+	 * Get the fields displayed by the resource.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return array
+	 */
+	public function fields(Request $request)
+	{
+		return [
+			ID::make()->sortable(),
+			Text::make(__('Code'), 'code')->required()->default(str_random()),
+			Number::make(__('Percentage'), 'percentage')->required(),
+			DateTime::make(__('Expires'), 'expires')->required()->default(today()->addDays(5)),
+		];
+	}
+
+	/**
+	 * Get the cards available for the request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return array
+	 */
+	public function cards(Request $request)
+	{
+		return [];
+	}
+
+	/**
+	 * Get the filters available for the resource.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return array
+	 */
+	public function filters(Request $request)
+	{
+		return [];
+	}
+
+	/**
+	 * Get the lenses available for the resource.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return array
+	 */
+	public function lenses(Request $request)
+	{
+		return [];
+	}
+
+	/**
+	 * Get the actions available for the resource.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return array
+	 */
+	public function actions(Request $request)
+	{
+		return [];
+	}
+}
---
+++ database/factories/DiscountFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Discount;
+use Faker\Generator as Faker;
+
+$factory->define(Discount::class, function (Faker $faker) {
+	return [
+		'percentage' => 10,
+		'code' => str_random(8),
+		'expires' => $faker->dateTime
+	];
+});
