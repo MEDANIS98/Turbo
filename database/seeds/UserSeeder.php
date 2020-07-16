0254c8d4e518dc01fbe64b76f23913208bd25d8b
CaddyDz <caddydz4@gmail.com>
2020-07-16 18:58:29 +0100

    Show brands in admin panel
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,7 +13,7 @@ class UserSeeder extends Seeder
 	 *
 	 * @return void
 	 */
-	public function run()
+	public function run(): void
 	{
 		$admin = User::create([
 			'name' => config('site.admin.name'),
