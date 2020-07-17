40e728d03453993939abd41a2a49e7418157ebde
CaddyDz <caddydz4@gmail.com>
2020-07-17 19:34:23 +0100

    Apply CS fixes
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- app/Nova/Stock.php
+++ app/Nova/Stock.php
@@ -6,9 +6,9 @@ namespace App\Nova;
 
 use Laravel\Nova\Fields\ID;
 use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\BelongsTo;
-use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
 class Stock extends Resource
